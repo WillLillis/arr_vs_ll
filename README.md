dbd93a79e6b4a39193c6f05a429e32e7f0de0fbc
WillLillis <wlillis@umass.edu>
2024-02-18 22:42:47 -0500

    Add README, time in seconds to reporting

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # arr_vs_ll
 
 After watching a [talk](https://www.youtube.com/watch?v=YQs6IC-vgmo&ab_channel=AlessandroStamatto) by Bjarne Stroustrup 
-and later a [recreation](https://www.youtube.com/watch?v=0jiP4pOoG8g&t=3528s&ab_channel=TheVimeagen) of the benchmark in TypeScript by the Primagen
+and later a [recreation](https://www.youtube.com/watch?v=0jiP4pOoG8g&t=3528s&ab_channel=TheVimeagen) of the benchmark in TypeScript by the Primeagen
 I wanted to redo the benchmark myself in C. 
 
 This repo is just a quick and dirty recreation of the comparison, and its results roughly line up with what was expected given the results of the other two videos.
+
+![](https://github.com/WillLillis/arr_vs_ll/blob/master/ArrayVsLinkedListGraph.png)
--- src/src.c
+++ src/src.c
@@ -1,23 +1,21 @@
-#include<stdio.h>
-#include<stdint.h>
-#include<stdbool.h>
-#include<stdlib.h>
-#include<assert.h>
-#include<string.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include<time.h>
-
+#include <time.h>
 
 typedef struct LNode {
     int32_t val;
-    struct LNode* prev;
-    struct LNode* next;
+    struct LNode *prev;
+    struct LNode *next;
 } LNode_t;
 
-LNode_t* createNode(int32_t val)
-{
-    LNode_t* node = (LNode_t*)malloc(sizeof(LNode_t));
-    if(node == NULL) {
+LNode_t *createNode(int32_t val) {
+    LNode_t *node = (LNode_t *)malloc(sizeof(LNode_t));
+    if (node == NULL) {
         printf("Memory allocation error! Exiting...\n");
         exit(EXIT_FAILURE);
     }
@@ -29,42 +27,40 @@ LNode_t* createNode(int32_t val)
     return node;
 }
 
-LNode_t* cutNode(LNode_t* head, size_t index)
-{
-    LNode_t* curr = head;
+LNode_t *cutNode(LNode_t *head, size_t index) {
+    LNode_t *curr = head;
     bool is_head = index == 0;
 
-    while(index > 0 && curr != NULL) {
+    while (index > 0 && curr != NULL) {
         curr = curr->next;
         index--;
     }
 
-    if(curr == NULL) {
+    if (curr == NULL) {
         printf("Ya done messed up\n");
         exit(EXIT_FAILURE);
     }
 
-    LNode_t* prev = (LNode_t*)curr->prev;
-    LNode_t* next = (LNode_t*)curr->next;
+    LNode_t *prev = (LNode_t *)curr->prev;
+    LNode_t *next = (LNode_t *)curr->next;
     free(curr);
-    
-    if(prev != NULL) {
-        prev->next = (struct LNode*)next;
+
+    if (prev != NULL) {
+        prev->next = (struct LNode *)next;
     }
-    if(next != NULL) {
-        next->prev = (struct LNode*)prev;
+    if (next != NULL) {
+        next->prev = (struct LNode *)prev;
     }
 
     return is_head ? next : head;
 }
 
-LNode_t* createLL(size_t size)
-{
-    LNode_t* head = createNode(0);
-    LNode_t* curr = head;
-    LNode_t* tmp;
+LNode_t *createLL(size_t size) {
+    LNode_t *head = createNode(0);
+    LNode_t *curr = head;
+    LNode_t *tmp;
 
-    for(int i = 1; i < size; i++) {
+    for (int i = 1; i < size; i++) {
         tmp = createNode(i);
         curr->next = tmp;
         tmp->prev = curr;
@@ -74,12 +70,11 @@ LNode_t* createLL(size_t size)
     return head;
 }
 
-void traverse_and_print(LNode_t* head)
-{
-    LNode_t* curr = head;
-    size_t index = 0; 
+void traverse_and_print(LNode_t *head) {
+    LNode_t *curr = head;
+    size_t index = 0;
 
-    while(curr != NULL) {
+    while (curr != NULL) {
         printf("(%zu) Val: %d\n", index, curr->val);
         curr = curr->next;
         index++;
@@ -87,100 +82,99 @@ void traverse_and_print(LNode_t* head)
 }
 
 typedef struct lenArray {
-    int32_t* arr;
+    int32_t *arr;
     size_t len;
     size_t capacity;
 } lenArray_t;
 
-void lenArray_init(lenArray_t* arr, size_t size)
-{
-    if(arr == NULL) {
+void lenArray_init(lenArray_t *arr, size_t size) {
+    if (arr == NULL) {
         printf("Invalid address for arr passed in!\n");
         return;
     }
 
-    arr->arr = (int32_t*)malloc(size * sizeof(int32_t));
-    if(arr->arr == NULL) {
+    arr->arr = (int32_t *)malloc(size * sizeof(int32_t));
+    if (arr->arr == NULL) {
         arr->len = arr->capacity = 0;
         printf("Allocation error!\n");
         return;
     } else {
         arr->len = arr->capacity = size;
-        for(int i = 0; i < arr->len; i++) {
+        for (int i = 0; i < arr->len; i++) {
             arr->arr[i] = i;
         }
     }
 }
 
-void cut_index(lenArray_t* arr, size_t index)
-{
-    if(arr == NULL) {
+void cut_index(lenArray_t *arr, size_t index) {
+    if (arr == NULL) {
         printf("Invalid address for arr passed in!\n");
         return;
     }
 
     assert(index < arr->capacity);
-    
-    memmove(&(arr->arr[index]), &(arr->arr[index + 1]), (arr->len - index - 1) * sizeof(int32_t)); 
+
+    memmove(&(arr->arr[index]), &(arr->arr[index + 1]),
+            (arr->len - index - 1) * sizeof(int32_t));
 
     arr->len--;
 }
 
-void print_arr(lenArray_t arr)
-{
-    for(int i = 0; i < arr.len; i++) {
+void print_arr(lenArray_t arr) {
+    for (int i = 0; i < arr.len; i++) {
         printf("(%d) %d\n", i, arr.arr[i]);
     }
 }
 
-
-int main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
     srand(time(NULL));
 
     size_t size;
-    if(argc < 2) {
+    if (argc < 2) {
         size = 100;
     } else {
         size = atoi(argv[1]);
         // could put some additional checks here
-        // but we're just doing a basic test, 
+        // but we're just doing a basic test,
         // no need to over do it
     }
 
     printf("Size: %zu\n", size);
 
-
     int n_removes = size * 0.1;
-    size_t* remove_idxs = (size_t*)malloc(n_removes * sizeof(size_t));
-    if(remove_idxs == NULL) {
+    size_t *remove_idxs = (size_t *)malloc(n_removes * sizeof(size_t));
+    if (remove_idxs == NULL) {
         printf("Sadness\n");
         exit(EXIT_FAILURE);
     }
 
-    for(int i = 0; i < n_removes; i++) {
+    for (int i = 0; i < n_removes; i++) {
         remove_idxs[i] = rand() % (size - i);
     }
 
-    LNode_t* head = createLL(size);
-    
+    LNode_t *head = createLL(size);
+
     lenArray_t arr;
     lenArray_init(&arr, size);
-    
+
     clock_t LL_start = clock();
-    for(int i = 0; i < n_removes; i++) {
+    for (int i = 0; i < n_removes; i++) {
         head = cutNode(head, remove_idxs[i]);
     }
     clock_t LL_stop = clock();
 
     clock_t arr_start = clock();
-    for(int i = 0; i < n_removes; i++) {
+    for (int i = 0; i < n_removes; i++) {
         cut_index(&arr, remove_idxs[i]);
     }
     clock_t arr_stop = clock();
+    uintmax_t LL_ticks = LL_stop - LL_start;
+    uintmax_t arr_ticks = arr_stop - arr_start;
 
-    printf("\tLL time: %ju\n", (uintmax_t)(LL_stop - LL_start));
-    printf("\tArr time: %ju\n", (uintmax_t)(arr_stop - arr_start));
-    
+    printf("\tLL ticks: %ju, time: %lf\n", LL_ticks,
+           (double)LL_ticks / CLOCKS_PER_SEC);
+    printf("\tArr ticks: %ju, time: %lf\n", arr_ticks,
+           (double)arr_ticks / CLOCKS_PER_SEC);
+
     return 0;
 }
